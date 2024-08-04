9f3100ad8fc7b46fc8b43af609338237f362a9a4
Vladislav Sirotkin <vladislav.sirotkin@jti.com>
2024-08-04 17:41:11 +0300

    add route for auth token

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
+from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
-from cats.views import CatViewSet, OwnerViewSet, LightCatViewSet
+from cats.views import CatViewSet, LightCatViewSet, OwnerViewSet
 
 router = DefaultRouter()
 router.register('cats', CatViewSet)
@@ -10,4 +11,5 @@ router.register(r'mycats', LightCatViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
